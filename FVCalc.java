0c5ef213ba8c867e25215a21294aa5b306f36335
chenlupo <chen.lupo@post.runi.ac.il>
2024-11-13 13:55:57 +0200

    completed

--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,11 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
+		int currentValue = Integer.parseInt(args[0]);
+		double intrest = Double.parseDouble(args[1]);
+        double rate = intrest/100.0;
+		double years = Double.parseDouble(args[2]);
+		double futureValue = currentValue * Math.pow((1 + rate), years);
+		System.out.print("After " + years + " years, $" + currentValue + " saved at " + intrest + " % will yield $" + (int)futureValue);
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,11 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+
+	int num = Integer.parseInt(args[0]);
+	int ones = num % 10;
+	int tens = ((num-ones)%100)/10;
+	int hundreds = num/100;
+	System.out.println(hundreds + "  hundreds, " + tens + " tens, and " + ones + " ones.");
 	}
 }
